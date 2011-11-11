66589524747ab9784562ec1a8a46fb982b296653
Wes <wwnolte@gmail.com>
2011-11-11 17:51:44 +0000

    Initial commit

---
+++ jquery.jOrgChart.js
@@ -0,0 +1,107 @@
+(function($) {
+
+  $.fn.jOrgChart = function(options) {
+    var opts = $.extend({}, $.fn.jOrgChart.defaults, options);
+	var $appendTo = $(opts.chartElement);
+
+    return this.each(function() {
+      $this = $(this);
+      var $container = $("<div class='" + opts.chartClass + "'/>");
+      if($this.is("ul")) {
+        buildNode($this.find("li:first"), $container, 0, opts);
+      }
+      else if($this.is("li")) {
+        buildNode($this, $container, 0, opts);
+      }
+      $appendTo.append($container);
+    });
+  };
+
+  $.fn.jOrgChart.defaults = {
+	chartElement : 'body',
+    depth      : -1,
+    chartClass : "jOrgChart"
+  };
+
+  function buildNode($node, $appendTo, level, opts) {
+	
+    var $table = $("<table cellpadding='0' cellspacing='0' border='0'/>");
+    var $tbody = $("<tbody/>");
+
+	// Construct the node container(s)
+    var $nodeRow = $("<tr/>").addClass("node-cells");
+    var $nodeCell = $("<td/>").addClass("node-cell").attr("colspan", 2);
+    var $childNodes = $node.children("ul:first").children("li");
+	
+    if($childNodes.length > 1) {
+      $nodeCell.attr("colspan", $childNodes.length * 2);
+    }
+	// Draw the node
+	// Get the contents - any markup except li and ul allowed
+	var $nodeContent = $node.clone().children("ul,li").remove().end().html();
+    //var $heading = $("<h2>").text(nodeContent);
+    $nodeDiv = $("<div>").addClass("node").append($nodeContent);
+
+	// Expand and contract nodes
+    $nodeDiv.click(function() {
+      var $this = $(this);
+      var $tr = $this.closest("tr");
+      $tr.nextAll("tr").fadeToggle("fast");
+
+	  if($tr.hasClass('contracted')){
+		$this.css('cursor','n-resize');
+		$tr.addClass('expanded');
+	  }else{
+		$this.css('cursor','s-resize');
+		$tr.addClass('contracted');
+	  }
+    });
+	
+    $nodeCell.append($nodeDiv);
+    $nodeRow.append($nodeCell);
+    $tbody.append($nodeRow);
+
+    if($childNodes.length > 0) {
+	  // if it can be expanded then change the cursor
+	  $nodeDiv.css('cursor','n-resize').addClass('expanded');
+	
+	  // recurse until leaves found (-1) or to the level specified
+      if(opts.depth == -1 || (level+1 < opts.depth)) { 
+        var $downLineRow = $("<tr/>");
+        var $downLineCell = $("<td/>").attr("colspan", $childNodes.length*2);
+        $downLineRow.append($downLineCell);
+        
+		// draw the connecting line from the parent node to the horizontal line 
+		$downLine = $("<div></div>").addClass("line down");
+		$downLineCell.append($downLine);
+        $tbody.append($downLineRow);
+
+        // Draw the horizontal lines
+        var $linesRow = $("<tr/>");
+        $childNodes.each(function() {
+          var $left = $("<td/>").addClass("line left top");
+          var $right = $("<td/>").addClass("line right top");
+          $linesRow.append($left).append($right);
+        });
+
+		// horizontal line shouldn't extend beyond the first and last child branches
+        $linesRow.find("td:first").removeClass("top");
+        $linesRow.find("td:last").removeClass("top");
+        $tbody.append($linesRow);
+        var $childNodesRow = $("<tr/>");
+        $childNodes.each(function() {
+           var $td = $("<td class='node-container'/>");
+           $td.attr("colspan", 2);
+		   // recurse through children lists and items
+           buildNode($(this), $td, level+1, opts);
+           $childNodesRow.append($td);
+        });
+      }
+      $tbody.append($childNodesRow);
+    }
+
+    $table.append($tbody);
+    $appendTo.append($table);
+  };
+
+})(jQuery);
---
+++ readme.markdown
@@ -0,0 +1,87 @@
+#Readme
+
+
+##Overview
+
+Follow me [@wesnolte](http://twitter.com/wesnolte)
+
+jQuery OrgChart is a plugin that allows you to render structures with nested elements in a easy-to-read tree structure. To build the tree all you need is to make a single line call to the plugin and supply the HTML element Id for a nested unordered list element that is representative of the data you'd like to display.
+
+![jQuery OrgChart](http://i.imgur.com/2OpyG.png "jQuery OrgChart")
+
+----
+
+##Expected Markup & Example Usage
+
+To get up and running you'll need a few things. 
+
+-----
+
+###The JavaScript Libraries
+
+You need to include the jQuery as well as the jOrgChart libraries. For example:
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	<script src="jquery.jOrgChart.js"></script>
+
+----
+
+###The HTML
+
+You'll need to construct a nest unordered list that represents your node nesting. For example:
+
+	<ul id="org" style="display:none">
+	<li>
+	  Food
+	  <ul>
+	    <li>Beer</li>
+	    <li>Vegetables
+	      <ul>
+	        <li>Pumpkin</li>
+	        <li><a href="http://tquila.com" target="_blank">Aubergine</a></li>
+	      </ul>
+	    </li>
+	    <li>Bread</li>
+	    <li>Chocolate
+	      <ul>
+	        <li>Topdeck</li>
+	        <li>Reese's Cups</li>
+	      </ul>
+	    </li>
+	  </ul>
+	</li>
+	</ul>
+
+-----
+
+###The jQuery Call
+
+And the cherry on the top is the usual call, often but not always on document load. You'll need to specify the Id of the list in this call. For example:
+
+	jQuery(document).ready(function() {
+	    $("#org").jOrgChart();
+	});
+	
+This call will append the markup for the OrgChart to the `<body>` element by default, but you can specify this as part of the options.
+
+----
+
+##Demo
+
+You can view a demo of this here.
+
+------
+
+##Sourcecode
+
+Source code with an example is available here.
+
+-----
+
+##Configuration
+
+There are only 3 configuration options.
+
+1. **chartElement** - used to specify which HTML element you'd like to append the OrgChart markup to. -[default='body']-
+2. **depth** - tells the code what depth to parse to. The default value of "-1" instructs it to parse like it's 1999. -[default=-1]-
+3. **chartClass** - the name of the style class that is assigned to the generated markup. -[default='jOrgChart']-
